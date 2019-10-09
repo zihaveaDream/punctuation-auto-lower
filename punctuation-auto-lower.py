3dbc34ac57c4b6fd0db6f78a35e21e7d8b3da5ce
wangjiayi <wangjiayi@aobi.com>
2019-10-09 15:32:55 +0800

    init

---
+++ punctuation-auto-lower.py
@@ -0,0 +1,43 @@
+import sublime
+import sublime_plugin
+import re
+
+MAP_PUNCTUATION = {
+    "　": " ",
+    "“": '"',
+    "”": '"',
+    "！": "!",
+    "￥": "$",
+    "……": "^",
+    "（": "(",
+    "）": ")",
+    "——": "--",
+    "【": "[",
+    "】": "]",
+    "；": ";",
+    "’": "'",
+    "：": ":",
+    "，": ",",
+    "。": ".",
+    "、": ",",
+    "？": "?",
+}
+
+
+def changeContent(content):
+    for x in MAP_PUNCTUATION.items():
+        content = re.sub(x[0], x[1], content)
+    return content
+
+
+class PunctuationAutoLowerCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        r = sublime.Region(0, self.view.size())
+        content = self.view.substr(r)
+        self.view.replace(edit, r, changeContent(content))
+
+
+class PunctuationAutoLowerListener(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        if not "punctuation-auto-lower.py" in view.file_name():
+            view.run_command("punctuation_auto_lower")
